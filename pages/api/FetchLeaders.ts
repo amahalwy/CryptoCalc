2d049aa74bebdb3d2f0fbd61c70a9d33fc81c82d
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-03-10 02:15:24 -0500

    Change for try/catch block

--- pages/api/FetchLeaders.ts
+++ pages/api/FetchLeaders.ts
@@ -6,73 +6,67 @@ const prisma = new PrismaClient();
 
 const fetchLeaders = async (req, res) => {
   const prisma = new PrismaClient();
-  const tempLists: List[] = await prisma.list.findMany({
-    include: {
-      coins: true,
-    },
-  });
+  try {
+    const tempLists: List[] = await prisma.list.findMany({
+      include: {
+        coins: true,
+      },
+    });
 
-  if (!tempLists) {
-    return {
-      notFound: true,
-    };
-  }
-
-  tempLists.map(async (list) => {
-    let newTotal = 0;
-    await Promise.all(
-      list.coins.map(async (coin) => {
-        await fetchPrice(coin.name).then(async (res) => {
-          newTotal += res[coin.name.toLowerCase()].usd * coin.quantity;
-          try {
-            let diff: number = 0;
-            if (newTotal > list.total) {
-              diff = newTotal - list.total;
-            } else {
-              diff = list.total - newTotal;
+    tempLists.map(async (list) => {
+      let newTotal = 0;
+      await Promise.all(
+        list.coins.map(async (coin) => {
+          await fetchPrice(coin.name).then(async (res) => {
+            newTotal += res[coin.name.toLowerCase()].usd * coin.quantity;
+            try {
+              let diff: number = 0;
+              if (newTotal > list.total) {
+                diff = newTotal - list.total;
+              } else {
+                diff = list.total - newTotal;
+              }
+              await prisma.list.update({
+                where: {
+                  id: list.id,
+                },
+                data: {
+                  coins: {
+                    update: {
+                      where: {
+                        id: coin.id,
+                      },
+                      data: {
+                        price: {
+                          set: Number(res[coin.name.toLowerCase()].usd),
+                        },
+                      },
+                    },
+                  },
+                  currentTotal: Number(newTotal.toFixed(2)),
+                  percentChange: Number((diff / list.total).toFixed(4)),
+                },
+              });
+            } catch (error) {
+              res.status(400).json(error);
             }
+          });
+          try {
             await prisma.list.update({
               where: {
                 id: list.id,
               },
               data: {
-                coins: {
-                  update: {
-                    where: {
-                      id: coin.id,
-                    },
-                    data: {
-                      price: {
-                        set: Number(res[coin.name.toLowerCase()].usd),
-                      },
-                    },
-                  },
-                },
                 currentTotal: Number(newTotal.toFixed(2)),
-                percentChange: Number((diff / list.total).toFixed(4)),
               },
             });
           } catch (error) {
             res.status(400).json(error);
           }
-        });
-        try {
-          await prisma.list.update({
-            where: {
-              id: list.id,
-            },
-            data: {
-              currentTotal: Number(newTotal.toFixed(2)),
-            },
-          });
-        } catch (error) {
-          res.status(400).json(error);
-        }
-      })
-    );
-  });
+        })
+      );
+    });
 
-  try {
     const lists = await prisma.list.findMany({
       take: 10,
       orderBy: {
