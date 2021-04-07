67ce6d012fdc581b0797e9bc261bdd208c0454c9
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-04-07 09:53:01 -0700

    Created sub components instead for stack

--- components/IndexStack.tsx
+++ components/IndexStack.tsx
@@ -1,38 +1,64 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Button, Text } from "@chakra-ui/react";
 import { useRouter } from "next/router";
-import React from "react";
+import { Divider, Flex, useColorModeValue as mode } from "@chakra-ui/react";
+import * as React from "react";
+import { DescriptionProps, FeatureProps } from "../typescript/interfaces";
 
-const Feature: React.FC<{
-  title: string;
-  desc: string;
-  desc2: string;
-  [x: string]: any;
-}> = ({ title, desc, desc2, ...rest }) => {
+const Description: React.FC<DescriptionProps> = ({ value }) => {
+  return (
+    <Flex
+      as="dl"
+      direction={{ base: "column", sm: "row" }}
+      px="6"
+      py="4"
+      _even={{ bg: mode("gray.50", "gray.600") }}
+    >
+      <Box as="dd" flex="1">
+        {value}
+      </Box>
+    </Flex>
+  );
+};
+
+const Feature: React.FC<FeatureProps> = ({ title, desc, desc2, onClick }) => {
   return (
     <Box
-      p={5}
+      maxW={{ base: "xl", md: "7xl" }}
+      mx="auto"
+      px={{ md: "8" }}
       w={{ base: "100%", lg: "90%" }}
       h={{ base: "100%", lg: "430px", xl: "320px" }}
-      bg="white"
       shadow="md"
       cursor="pointer"
       borderWidth="1px"
-      borderRadius="md"
       mb={{ base: "20px", lg: "0" }}
-      {...rest}
     >
-      <Heading fontSize={{ base: 30, lg: 40 }}>{title}</Heading>
-      <Text mt={4} fontSize={{ base: 14, lg: 20 }}>
-        {desc}
-      </Text>
-      <Text mt={4} fontSize={{ base: 14, lg: 20 }}>
-        {desc2}
-      </Text>
+      <Box
+        maxW="3xl"
+        mx="auto"
+        rounded={{ md: "lg" }}
+        bg={mode("white", "gray.700")}
+        shadow="base"
+        overflow="hidden"
+      >
+        <Flex align="center" justify="space-between" px="6" py="4">
+          <Text as="h2" fontWeight="bold" fontSize="2xl" color="orange.400">
+            {title}
+          </Text>
+        </Flex>
+        <Divider />
+        <Box>
+          <Description value={desc} />
+        </Box>
+        <Box d="flex" justifyContent="center" mb="4%">
+          <Button children={desc2} onClick={onClick} />
+        </Box>
+      </Box>
     </Box>
   );
 };
 
-const StackEx = () => {
+const IndexStack = () => {
   const router = useRouter();
   return (
     <Box
@@ -40,23 +66,21 @@ const StackEx = () => {
       m="0 auto"
       alignItems="center"
       w={{ base: "90%", lg: "100%" }}
-      d="block"
     >
       <Feature
         title="New Portfolio"
-        desc="Create a portfolio with available crypto currencies and a $1 million budget. How much money can you make?"
-        desc2="Get started here and challenge the leaderboards!"
+        desc="Create a crypto-currency portfolio with a $1 million budget. How much money can you make in 3 days?"
+        desc2="Get started"
         onClick={() => router.push("calc")}
-        mb="20px"
       />
       <Feature
         title="Portfolio Search"
-        desc="See how you wage against others. If you've already made a portfolio, check out how much you've made so far!"
-        desc2="You have 3 total days before a final value change is registered. Goodluck!"
+        desc="See how you wage against others. If you've already made a portfolio, check out how much you've made so far."
+        desc2="Search"
         onClick={() => router.push("portfolio")}
       />
     </Box>
   );
 };
 
-export default StackEx;
+export default IndexStack;
