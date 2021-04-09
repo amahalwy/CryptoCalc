f34d5f95cf2fbd5a34edf1e27069015725638b0a
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-04-08 22:34:02 -0700

    Changed box to flex to minimize redundancy

--- components/CountDown.tsx
+++ components/CountDown.tsx
@@ -1,21 +1,19 @@
 import React from "react";
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import { CountdownProps } from "../typescript/interfaces";
 
-const CountDown: React.FC<CountdownProps> = ({ timerComponents }) => {
-  return (
-    <Box d="flex" w="100%" justifyContent="center" pb="10px">
-      {timerComponents.length ? (
-        timerComponents
-      ) : (
-        <Box>
-          <Text fontSize={30} color="white">
-            Time's up!
-          </Text>
-        </Box>
-      )}
-    </Box>
-  );
-};
+const CountDown: React.FC<CountdownProps> = ({ timerComponents }) => (
+  <Flex w="100%" justifyContent="center" pb="10px">
+    {timerComponents.length ? (
+      timerComponents
+    ) : (
+      <Box>
+        <Text fontSize={30} color="white">
+          Time's up!
+        </Text>
+      </Box>
+    )}
+  </Flex>
+);
 
 export default CountDown;
