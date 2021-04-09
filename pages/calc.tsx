ff27855375cdbeb72d45e9e6de413acc200bd8aa
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-04-08 22:37:36 -0700

    Changes for styling of calc

--- pages/calc.tsx
+++ pages/calc.tsx
@@ -4,38 +4,36 @@ import SearchForm from "../components/SearchForm";
 import SearchCoin from "../components/SearchCoin";
 import Watchlist from "../components/Watchlist";
 import GoogleAnalytics from "../components/GoogleAnalytics";
+import { Coin } from "../typescript/interfaces";
 
 const Calc: React.FC = () => {
   const [coin, setCoin] = React.useState<null | any>(null);
   const [total, setTotal] = React.useState<null | number>(0);
-  const [watchlist, setWatchlist] = React.useState<[]>([]);
+  const [watchlist, setWatchlist] = React.useState<Coin[]>([]);
   const [calculatingTotal, setCalculatingTotal] = React.useState<boolean>(
     false
   );
 
   return (
-    <Box h="100%" w="100%">
+    <Box>
       <GoogleAnalytics />
       <Box
         w={{ base: "90%", lg: "60%" }}
-        m="4% auto"
+        m="6% auto"
         bg="white"
         borderWidth="1px"
         borderRadius="md"
       >
-        <Box m="10px 0">
-          <Box ml="1%">
-            <Heading fontSize={{ base: 44, lg: 54 }} color="orange.400">
-              CryptoCalc
-            </Heading>
-            <Heading fontSize={24} color="orange.400">
+        <Box px={4} py={4}>
+          <Box color="orange.400">
+            <Heading fontSize={{ base: 36, lg: 54 }}>CryptoCalc</Heading>
+            <Heading fontSize={18}>
               How much can you make with 1 million?
             </Heading>
           </Box>
         </Box>
         <Box>
           <SearchForm setCoin={setCoin} />
-
           {!coin ? null : coin && !coin.error ? (
             <SearchCoin
               coin={coin}
@@ -43,11 +41,10 @@ const Calc: React.FC = () => {
               setWatchlist={setWatchlist}
             />
           ) : (
-            <Box ml="10px" color="red">
+            <Box px={4} py={2} color="red">
               Error: {coin.error}
             </Box>
           )}
-
           {watchlist.length > 0 ? (
             <Box>
               <Watchlist
