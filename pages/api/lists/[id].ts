6d8f3d5d9d7773584ae9a84e5a929cd8386e68d5
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-04-10 22:27:38 -0700

    Created API handlers for differing HTTP methods

--- pages/api/lists/[id].ts
+++ pages/api/lists/[id].ts
@@ -1,8 +1,13 @@
 import { List } from "@prisma/client";
 import prisma from "../../../lib/prisma";
+import { Coin } from "../../../typescript/interfaces";
 
 const handler = async (
-  req: { method: any; body: { name: any } },
+  req: {
+    method: any;
+    query: { id: string };
+    body: { name: any; localCoins: Coin[]; list: { id: any } };
+  },
   res: {
     status: (
       arg0: number
@@ -10,42 +15,105 @@ const handler = async (
       (): any;
       new (): any;
       json: {
-        (arg0: { status: number; error?: string; id?: string }): any;
+        (arg0: { status: number; error?: string; list?: List }): any;
         new (): any;
       };
     };
+    json: (arg0: List) => any;
   }
 ) => {
-  const list: List = await prisma.list.findFirst({
-    where: {
-      owner: {
-        name: req.body.name,
-      },
-    },
-    include: {
-      coins: true,
-    },
-  });
+  switch (req.method) {
+    case "GET":
+      if (req.query) {
+        const list: List = await prisma.list.findFirst({
+          where: {
+            owner: {
+              name: req.query.id,
+            },
+          },
+          include: {
+            coins: true,
+          },
+        });
 
-  if (!list) {
-    const response: {
-      status: number;
-      error: string;
-    } = {
-      status: 404,
-      error: "Error: List not found",
-    };
+        if (!list) {
+          const response: {
+            status: number;
+            error: string;
+          } = {
+            status: 404,
+            error: "Error: List not found",
+          };
 
-    return res.status(404).json(response);
-  } else {
-    const response: {
-      status: number;
-      list: List;
-    } = {
-      status: 200,
-      list: list,
-    };
-    return await res.status(200).json(response);
+          return res.status(404).json(response);
+        } else {
+          const response: {
+            status: number;
+            list: List;
+          } = {
+            status: 200,
+            list: list,
+          };
+          return await res.status(200).json(response);
+        }
+      } else {
+      }
+      const list: List = await prisma.list.findFirst({
+        where: {
+          owner: {
+            name: req.body.name,
+          },
+        },
+        include: {
+          coins: true,
+        },
+      });
+
+      if (!list) {
+        const response: {
+          status: number;
+          error: string;
+        } = {
+          status: 404,
+          error: "Error: List not found",
+        };
+
+        return res.status(404).json(response);
+      } else {
+        const response: {
+          status: number;
+          list: List;
+        } = {
+          status: 200,
+          list: list,
+        };
+        return await res.status(200).json(response);
+      }
+    case "PATCH":
+      const getTotal = (coins: Coin[]) => {
+        return coins.reduce((acc: number, curr: Coin) => {
+          return acc + curr.market_data.current_price.usd * curr.quantity;
+        }, 0);
+      };
+      const newTotal = getTotal(req.body.localCoins);
+      try {
+        const list: List = await prisma.list.update({
+          where: {
+            id: req.body.list.id,
+          },
+          data: {
+            endTotal: newTotal,
+            active: false,
+          },
+          include: {
+            coins: true,
+          },
+        });
+
+        return await res.json(list);
+      } catch (error) {
+        return res.status(404).json(error);
+      }
   }
 };
 
---
+++ pages/api/lists/index.ts
@@ -0,0 +1,184 @@
+import { List, User } from "@prisma/client";
+import { uuid } from "uuidv4";
+import prisma from "../../../lib/prisma";
+import { fetchPrice } from "../../../util/coins/price";
+import { Coin } from "../../../typescript/interfaces";
+
+interface Response {
+  status?: number;
+  active: boolean;
+  name: string;
+  otp: string;
+  id: string;
+  userId: string;
+  startDate: string;
+  endDate: string;
+  total: number;
+}
+
+const handler = async (req, res) => {
+  switch (req.method) {
+    case "GET":
+      // Leaders
+      try {
+        const tempLists = await prisma.list.findMany({
+          include: {
+            coins: true,
+          },
+        });
+
+        if (!tempLists) {
+          return {
+            notFound: true,
+          };
+        }
+
+        tempLists.map(async (list) => {
+          let newTotal = 0;
+          let coinsString = [];
+          let diff: number = 0;
+
+          list.coins.map(async (coin) => {
+            coinsString.push(coin.name.toLowerCase());
+            const requestParam = coinsString.join(",");
+
+            await fetchPrice(requestParam)
+              .then(async (res) => {
+                newTotal += res[coin.name.toLowerCase()].usd * coin.quantity;
+                return res;
+              })
+              .then(async (res) => {
+                if (newTotal > list.total) {
+                  diff = newTotal - list.total;
+                } else {
+                  diff = list.total - newTotal;
+                }
+                await prisma.list.update({
+                  where: {
+                    id: list.id,
+                  },
+                  data: {
+                    coins: {
+                      update: {
+                        where: {
+                          id: coin.id,
+                        },
+                        data: {
+                          price: {
+                            set: Number(res[coin.name.toLowerCase()].usd),
+                          },
+                        },
+                      },
+                    },
+                    currentTotal: Number(newTotal.toFixed(2)),
+                    percentChange: Number((diff / list.total).toFixed(4)),
+                  },
+                });
+              });
+          });
+        });
+
+        const lists: List[] = await prisma.list.findMany({
+          take: 10,
+          orderBy: {
+            percentChange: "desc",
+          },
+          include: {
+            coins: true,
+            owner: true,
+          },
+        });
+        const response: {
+          lists: List[];
+          status: number;
+        } = {
+          status: 200,
+          lists,
+        };
+
+        return res.json(response);
+      } catch (error) {
+        const response = {
+          status: 400,
+          ...error,
+        };
+
+        return res.json(response);
+      }
+    case "POST":
+      const name: string = req.body.name;
+      const coins: Coin[] = req.body.coins;
+      const total: number = coins.reduce((acc, curr) => {
+        return acc + curr.market_data.current_price.usd * curr.quantity;
+      }, 0);
+
+      // result.setDate(result.getDate() + 3);
+      const endDate = (date: number) => {
+        let result = new Date(date);
+        let current = new Date(date);
+
+        // result.setDate(result.getMinutes() + 1);
+        // return result;
+        return new Date(date + 20000);
+      };
+
+      const start: string = Date.now().toString();
+      const end: string = endDate(Date.now()).toString();
+
+      try {
+        const newUser: User = await prisma.user.create({
+          data: {
+            id: uuid(),
+            name,
+            otp: uuid(),
+          },
+        });
+
+        const newList: List = await prisma.list.create({
+          data: {
+            id: uuid(),
+            userId: newUser.id,
+            startDate: start,
+            endDate: end,
+            total,
+            currentTotal: total,
+            percentChange: 0,
+          },
+        });
+
+        await Promise.all(
+          coins.map(async (coin) => {
+            return prisma.coin.create({
+              data: {
+                name: coin.name,
+                listId: newList.id,
+                quantity: coin.quantity || 0,
+                price: coin.market_data.current_price.usd,
+              },
+            });
+          })
+        );
+
+        const updatedList: List = await prisma.list.findUnique({
+          where: { id: newList.id },
+          include: {
+            coins: true,
+          },
+        });
+
+        const response: Response = {
+          status: 201,
+          active: req.body.active,
+          name: newUser.name,
+          otp: newUser.otp,
+          ...updatedList,
+        };
+
+        return res.json(response);
+      } catch (err) {
+        err.status = 400;
+        return res.status(400).json(err);
+      }
+  }
+};
+export default handler;
