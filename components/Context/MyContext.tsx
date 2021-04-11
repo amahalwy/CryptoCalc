e5e2fbe3b7e5b4e2b7edf736d79441852139d4fb
Ahmed El Mahallawy <amahalwy@gmail.com>
2021-04-10 23:09:30 -0700

    Removed/changed files; added moduels for package.json

--- components/Context/MyContext.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { Context } from "../../typescript/interfaces";
-const MyContext = React.createContext<Context>({});
-export const MyProvider = MyContext.Provider;
-export default MyContext;
--- pages/api/FetchAllCoins.ts
+++
@@ -1,9 +0,0 @@
-export const fetchCoins = () => {
-  return fetch(process.env.NEXT_PUBLIC_RAPID_API_FETCH_COINS, {
-    method: "GET",
-    headers: {
-      "x-rapidapi-key": process.env.NEXT_PUBLIC_RAPID_API_KEY,
-      "x-rapidapi-host": process.env.NEXT_PUBLIC_RAPID_API_HOST,
-    },
-  });
-};
